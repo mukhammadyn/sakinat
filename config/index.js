728dbbbbd497cc5ab585143e34cb090cbd2482a5
akhmadiy <akhmadiy@abutech.uz>
2021-12-14 13:57:44 +0500

    add envoirment variables

--- config/index.js
+++ config/index.js
@@ -1,3 +1,10 @@
-module.exports = {
-  port:3000
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV === "test") {
+  dotenv.config({ path: ".env.test" });
+} else {
+  dotenv.config();
 }
+module.exports = {
+  port: process.env.APP_PORT || 5000,
+};
