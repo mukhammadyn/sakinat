201cafa28a98af2655a7007485419e4cf051067b
akhmadiy <akhmadiy@abutech.uz>
2021-12-14 13:54:59 +0500

    refactor models

--- src/models/User.js
+++ src/models/User.js
@@ -7,14 +7,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      models.User.hasOne(models.Baskets, {
-        foreignKey: 'UserId',
-        as: 'users',
-        onDelete: 'cascade',
-        hooks: true
-      });
-    }
+  
   }
 
   User.init(
