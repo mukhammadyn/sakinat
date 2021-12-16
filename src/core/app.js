37085bf48c4dc6a324a5814e6aa2a3021531c9f9
Mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2021-12-16 18:52:30 +0500

    add general styles

--- src/core/app.js
+++ src/core/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('./../public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
